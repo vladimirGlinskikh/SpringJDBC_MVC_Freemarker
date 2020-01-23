9749f79c43c28f148f61c0fbdc3705031478a597
VladimirGlinskikh <support@zhelezyaka.kz>
2020-01-23 21:59:20 +0600

    created methods save, delete, update

--- src/main/java/kz/zhelezyaka/dao/UserDao.java
+++ src/main/java/kz/zhelezyaka/dao/UserDao.java
@@ -6,5 +6,13 @@ import java.util.List;
 
 public interface UserDao {
 
+    void save(User user);
+
+    User getById(int id);
+
     List<User> findAll();
+
+    void update(User user);
+
+    void delete(int id);
 }
