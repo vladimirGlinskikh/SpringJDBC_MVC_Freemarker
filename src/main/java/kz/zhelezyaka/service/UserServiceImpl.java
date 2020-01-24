181315aed2911eac84b32a644e19a2760c74344a
VladimirGlinskikh <support@zhelezyaka.kz>
2020-01-24 18:30:28 +0600

    created methods save, delete, update in UserService

--- src/main/java/kz/zhelezyaka/service/UserServiceImpl.java
+++ src/main/java/kz/zhelezyaka/service/UserServiceImpl.java
@@ -16,4 +16,20 @@ public class UserServiceImpl implements UserService {
     public List<User> findAll() {
         return userDao.findAll();
     }
+
+    public void save(User user) {
+        userDao.save(user);
+    }
+
+    public User getById(int id) {
+        return userDao.getById(id);
+    }
+
+    public void update(User user) {
+        userDao.update(user);
+    }
+
+    public void delete(int id) {
+        userDao.delete(id);
+    }
 }
